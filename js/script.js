a5ae81d2657c99b5ba1c9ab270727c3c01c4c243
anazirjonov <anazirjonov10@gmail.com>
2024-01-13 16:38:45 +0500

    new

---
+++ js/script.js
@@ -0,0 +1,84 @@
+// ex1
+
+// let number = +prompt("ixtiyori sonni kiriting");
+// if (number > 0) {
+//   document.write(number + " ushbu son musbat");
+// }
+//  else {
+//   document.write(number + " ushbu son manfiy");
+// }
+
+// ex2
+// let = number = prompt("Ixtiyoriy sonni kiriting");
+// if (number >= 90 && number <= 100) {
+//   console.log("A");
+// } else if (number >= 80 && number <= 89) {
+//   console.log("B");
+// } else if (number >= 70 && number <= 79) {
+//   console.log("C");
+// } else if (number >= 60 && number <= 69) {
+//   console.log("D");
+// } else if (number >= 0 && number <= 59) {
+//   console.log("F");
+// } else {
+//   console.log("G");
+// }
+
+// const year = "soni kiriting";
+// if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//   console.log("Kabisa yili!");
+// } else {
+//   console.log("Kabisa yili emas!");
+// }
+
+// let numberR = +prompt("4-masala:Ixtiyoriy sonni kiriting");
+// if (numberR > 0) {
+//   console.log(numberR + " ushbu son musbat");
+// } else if (numberR < -0) {
+//   console.log(numberR + " ushbu son manfiy");
+// } else if (numberR == 0) {
+//   console.log(numberR + " ushbu son 0 ga teng");
+// }
+
+// ex-5
+
+// let a = 30;
+// let b = 30;
+
+// if (a > b) {
+//   console.log(a);
+// } else if (a < b) {
+//   console.log(b);
+// } else {
+//   console.log("teng");
+// }
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// if (a < b && a < c) {
+//   console.log(a);
+// } else if (b < a && b < c) {
+//   console.log(b);
+// } else if (c < a && c < b) {
+//   console.log(c);
+// } else {
+//   console.log("teng");
+// }
+
+let a = -10;
+let b = 10;
+let c = -5;
+if (a > 0) {
+  a = 0;
+}
+
+if (b > 0) {
+  b = 0;
+}
+
+if (c > 0) {
+  c = 0;
+}
+
+console.log(a + b + c);
